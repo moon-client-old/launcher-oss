eb4f775ab31bc6f6eb22c11c567badf9f3b46722
oad <48562304+oaddyk@users.noreply.github.com>
2023-05-29 17:48:12 +0200

    improve window config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,16 +6,10 @@ use crate::proprietary::{SerialProvideError, PROPRIETARY_LIBRARY};
 mod proprietary;
 mod storage;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
-    unsafe { println!("{:#?}", proprietary::fetch_serial()) }
+    // unsafe { println!("{:#?}", proprietary::fetch_serial()) }
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
